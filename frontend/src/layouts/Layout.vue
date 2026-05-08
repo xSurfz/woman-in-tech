f4136800b688bf44ab068a1d5962e0bc420b1a8f
Juan Giraldo <sebas311-@hotmail.com>
2026-05-08 17:31:28 -0500

    Layout, FOoter y NavBar añadida

---
+++ frontend/src/layouts/Layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Navbar />
+    <main class="flex-grow">
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Navbar from '../components/Navbar.vue'
+import Footer from '../components/Footer.vue'
+</script>
