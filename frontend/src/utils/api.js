7d29be55c62d550f13bfe47344402d27b9d61e80
Juan Giraldo <sebas311-@hotmail.com>
2026-05-08 19:06:39 -0500

    Pagina de programas añadido, junto a su llamado de API

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,5 @@
 export const fetchEvents = async () => {
   try {
-    // Trae la URL directamente del entorno de Vite
     const response = await fetch(`${import.meta.env.VITE_API_URL}/events.json`)
     
     if (!response.ok) throw new Error('Error cargando eventos')
@@ -10,3 +9,19 @@ export const fetchEvents = async () => {
     return { upcoming: [], past: [] }
   }
 }
+
+export const fetchPrograms = async () => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/programs.json`);
+    
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: No se pudo cargar programas`);
+    }
+    
+    return await response.json();
+  } catch (error) {
+    console.error('Error en fetchPrograms:', error);
+    // Retorna un array vacío para evitar errores de .map() en el componente
+    return [];
+  }
+};
